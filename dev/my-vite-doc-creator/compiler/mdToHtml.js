28215f0584f4dc2e32052c59b0a78aca32e3d48f
cfy <1263473716@qq.com>
2021-02-21 23:22:50 +0800

    markdown转换html2

--- dev/my-vite-doc-creator/compiler/mdToHtml.js
+++ dev/my-vite-doc-creator/compiler/mdToHtml.js
@@ -10,6 +10,9 @@ const {
 } = require('../libs/utils');
 
 const {
+  regexp: {
+    reg_mdStr
+  },
   outerPath: {
     mdPath,
     htmlPath
@@ -35,8 +38,16 @@ function mdToHtml (filename) {
   //读取markdown的模板html文件 -md.html
   let _htmlStr = readFile(htmlDir + '/md.html');
   //将markdown文件的内容通过markdown插件转换成html字符串
-  const newStr = markdown(_mdStr)
-  console.log(newStr);
+  const newStr = markdown(_mdStr);
+  //将模板md.html里的{{newStr}}替换成newStr(markdown插件转换出的结果)
+  _htmlStr = _htmlStr.replace(reg_mdStr, newStr);
+  
+  //将新的_htmlSrr写入html文件并保存到src/html目录下
+  writeFileSync(htmlPath + '/' + filename.replace('.md', '.html'), _htmlStr, function (err) {
+    if (err) {
+      throw new Error('File is failed to write', err);
+    }
+  })
 }
 
 module.exports = mdToHtml;
--- dev/my-vite-doc-creator/config/index.js
+++ dev/my-vite-doc-creator/config/index.js
@@ -46,7 +46,9 @@ const regexp = {
   //匹配header-title中的内容
   reg_headerTitleContent: /<h1 class="header-title">([\d\D]*?)<\/h1>/,
   //匹配iframe page中的内容
-  reg_iframeContent: /<div class="iframe-page">([\d\D]*?)<\/div>/
+  reg_iframeContent: /<div class="iframe-page">([\d\D]*?)<\/div>/,
+  //匹配md.html内部的{{newStr}}
+  reg_mdStr: /{{(.+?)}}/
 }
 
 module.exports = {
